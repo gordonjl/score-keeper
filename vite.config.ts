17dcac055b48494100e3badbc9ace3b293767023
John Gordon <john.gordon@pencilwrench.com>
2025-10-03 14:26:44 -0400

    chore: update Netlify build config and move tanstack plugin to devDependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
-    netlify(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 })
